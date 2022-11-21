2a115c1d9bf5b05df544d5e401aff48d8da0f93c
Sacramentix <benji.u1225u@gmail.com>
2022-11-21 03:46:42 +0100

    Generate Open Graph on vercel

---
+++ netlify/edge-functions/customOpenGraph.ts
@@ -0,0 +1,49 @@
+import { Context } from "netlify:edge";
+
+const langs = {
+    en : "Shortest path between...",//English
+    fr : "Chemin le plus court entre...",//French
+    de : "Kürzester Weg zwischen...",//German
+    it : "Il percorso più breve tra...",//Italian
+    eo : "Plej mallonga vojo inter...",//Esperanto
+} as const;
+  
+const langsKey = Object.keys(langs);
+
+type LangType = typeof langs;
+
+export default async (req: Request, ctx: Context) => {
+
+    const res:Response = await ctx.next();
+    if (res.headers.get("content-type") != "text/html") return res;
+    const url = new URL(req.url);
+    const p = url.pathname.endsWith("/") ? url.pathname.slice(0, -1) : url.pathname;
+    const [, lang, startS, endS, ...rest] = p.split(/\//);
+    if (rest.length > 0) return res;
+    if (!langsKey.includes(lang)) return res;
+
+    const [start, end] = [parseInt(startS), parseInt(endS)];
+    if (start <= 0 || end <= 0) return res;
+
+    // @ts-ignore
+    const description = langs[lang];
+    const ogUrl = `https://open-graph-six-degrees-of-wiki-adventure.vercel.app/api/og?lang=${lang}&start=${start}&end=${end}`;
+
+    const page = await res.text();
+    
+    const updated = page.replace('<!--OpenGraph-->',/*html*/`
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="Six Degrees of Wiki Adventure">
+        <meta name="twitter:description" content="${description}">
+        <meta name="twitter:image" content="${ogUrl}">
+        <meta name="twitter:image:alt" content="Thumbnail from Wikipédia" />
+        
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="Six Degrees of Wiki Adventure">
+        <meta property="og:description" content="${description}">
+        <meta property="og:image" content="${ogUrl}">
+        <meta property="og:image:alt" content="Thumbnail from Wikipédia" />
+    `)
+
+    return new Response(updated, res);
+};
